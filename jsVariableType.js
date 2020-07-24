8dc36fca2707ced542c81e6ea4af7bb22afc5b5a
Aman Kant <52783554+mnkant053305@users.noreply.github.com>
2020-07-25 01:15:47 +0530

    Create jsVariableType.js
    JavaScript code to display the variable types in JavaScript.

---
+++ jsVariableType.js
@@ -0,0 +1,7 @@
+var jsVarTypes = ['String', 25, true, , null];
+for (var i = 0; i < jsVarTypes.length; i++) {
+    if (i === 0) {
+        console.log('JavaScript Variable Types:');
+    }
+    console.log(typeof jsVarTypes[i]);
+}
