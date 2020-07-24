668e272347f24dde99d326d0af7f06cd439faa61
Aman Kant <52783554+mnkant053305@users.noreply.github.com>
2020-07-25 00:58:17 +0530

    Create calculateAge.js
    JavaScript code to calculate the age of an user. User needs to submit the year of birth.

---
+++ calculateAge.js
@@ -0,0 +1,14 @@
+var currentYear = new Date().getFullYear(); // get the current year
+var myBirthYear = window.prompt('Please enter your birth year : '); // ask the user to enter the year of birth
+
+// calcuate and display age of the user
+// display error in case the user inputs incorrent value.
+if (parseInt(myBirthYear) && parseInt(myBirthYear) < parseInt(currentYear) && Math.sign(myBirthYear) != '-1') {
+    var calcAge = currentYear - myBirthYear;
+    // display age of the user
+    window.alert('You are ' + calcAge + ' years old!');
+} else {
+    // display error message
+    window.alert('Please enter a valid birth year');
+}
+
