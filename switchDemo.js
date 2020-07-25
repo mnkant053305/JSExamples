421d90e9070ac714e64390ff21e58582e655f1c4
Aman Kant <52783554+mnkant053305@users.noreply.github.com>
2020-07-25 19:01:51 +0530

    JavaScript code to demo Switch logic.
    JavaScript code to demonstrate Switch logic!!!

---
+++ switchDemo.js
@@ -0,0 +1,19 @@
+var job = 'teacher'; // initialize job as teacher
+
+// switch demo
+switch (job) {
+    case 'teacher':
+    case 'Teacher': console.log('The user is an amazing ' + job);
+    break;
+
+    case 'student':
+    case 'Student': console.log('The user is an amazing ' + job);
+    break;
+
+    case 'driver':
+    case 'Driver': console.log('The user is an amazing ' + job);
+    break;
+
+    default: console.log('The user is an amazing JavaScript Programmer!');
+
+}
