43128779218ad58a03748a31a53fcef4454a8951
Aman Kant <52783554+mnkant053305@users.noreply.github.com>
2020-07-26 10:45:00 +0530

    JavaScript code to demonstrate Arrays
    Array Initialization Techniques.

---
+++ arrays.js
@@ -0,0 +1,18 @@
+// array initialization techniques
+var fruits = [];
+fruits = ['Apple', 'Mango', 'Banana'];
+
+
+var continents = ['Asia', 'Africa', 'Europe', 'North America', 'South America', 'Arctic', 'Antarctica'];
+
+// we should avoid the below initialization for arrays.
+var oceans = new Array('Indian Ocean', 'Pacific Ocean', 'Arctic Ocean', 'Antarctic Ocean', 'Atlantic Ocean');
+
+
+console.log(fruits, continents, oceans);
+console.log(fruits instanceof Array);
+console.log(continents instanceof Array);
+console.log(oceans instanceof Array);
+
+
+// array methods - push, pop, shift, unshift etc...
