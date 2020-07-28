f9d82c8195b1bc9558ac8d44cf9ca025c8009c17
Aman Kant <52783554+mnkant053305@users.noreply.github.com>
2020-07-28 14:45:03 +0530

    Updated relative path to absolute path for image source.
    Updated relative path to absolute path for image source.

--- domManipulation.js
+++ domManipulation.js
@@ -1,9 +1,9 @@
 // event listener
 document.querySelector('#switch-on').addEventListener('click',function () {
-    document.querySelector('#bulb').src = '../images/bulb_on.png'
+    document.querySelector('#bulb').src = 'https://github.com/mnkant053305/images/blob/master/bulb_on.png'
 });
 
 // anonymous function
 document.querySelector('#switch-off').addEventListener('click',function () {
-    document.querySelector('#bulb').src = '../images/bulb_off.png'
+    document.querySelector('#bulb').src = 'https://github.com/mnkant053305/images/blob/master/bulb_off.png'
 });
